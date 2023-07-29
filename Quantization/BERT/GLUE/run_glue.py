b35ac50d19c5b0bd213e9b41e2da36c0242c038f
DuLuZHU <84905941+DuLuZHU@users.noreply.github.com>
2023-07-29 11:46:12 +0800

    Update and rename Quantization/BERT/run_glue.py to Quantization/BERT/GLUE/run_glue.py

--- Quantization/BERT/GLUE/run_glue.py
+++ Quantization/BERT/GLUE/run_glue.py
@@ -46,6 +46,8 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 
+# Set up a single GPU training
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.30.0.dev0")
